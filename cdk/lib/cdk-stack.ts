b0a51e3380cfe17373b1d086b67b8f87ee4d4982
Ian Hovde <107483171+ianhovde@users.noreply.github.com>
2025-03-08 18:04:07 -0500

    move back to s3

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -32,20 +32,17 @@ export class CdkStack extends cdk.Stack {
     }));
 
     // Create an access key for the GitHub Actions IAM user
-    new cdk.aws_iam.CfnAccessKey(this, 'YbhovUIGitHubUserAccessKey', {
+    const accessKey = new cdk.aws_iam.CfnAccessKey(this, 'YbhovUIGitHubUserAccessKey', {
       userName: githubIamUser.userName,
     });
 
-    // Create Route53 record to point to the CloudFront distribution
-    const zone = cdk.aws_route53.HostedZone.fromLookup(this, 'ybhovHostedZone', {
-      domainName: 'ybhov.com',
+    // Output the access key and secret access key
+    new cdk.CfnOutput(this, 'YbhovUIGitHubUserAccessKeyId', {
+      value: accessKey.ref,
     });
-
-    // Get ARN of certificate from SSM
-    const certificateArn = cdk.aws_ssm.StringParameter.valueForStringParameter(this, '/ybhov/certificate/arn');
-
-    // Import cert from certificate manager for CloudFront
-    const certificate = cdk.aws_certificatemanager.Certificate.fromCertificateArn(this, 'ybhovCertificate', certificateArn)
+    new cdk.CfnOutput(this, 'YbhovUIGitHubUserSecretAccessKey', {
+      value: accessKey.attrSecretAccessKey,
+    });
 
     // Create CloudFront distribution
     const cloudFrontDistribution = new cdk.aws_cloudfront.CloudFrontWebDistribution(this, 'ybhovCloudFrontDistribution', {
@@ -56,10 +53,7 @@ export class CdkStack extends cdk.Stack {
           },
           behaviors: [{isDefaultBehavior: true}],
         },
-      ],
-      viewerCertificate: cdk.aws_cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
-        aliases: ['ybhov.com'],
-      }),
+      ]
     });
 
     // Grant the GitHub Actions IAM user access to the CloudFront distribution
@@ -68,12 +62,5 @@ export class CdkStack extends cdk.Stack {
       actions: ['cloudfront:CreateInvalidation'],
       resources: [`arn:aws:cloudfront::${this.account}:distribution/${cloudFrontDistribution.distributionId}`],
     }));
-
-    // Create Aaaa record in Route53 to point to the CloudFront
-    new cdk.aws_route53.AaaaRecord(this, 'ybhovARecord', {
-      recordName: 'ybhov.com',
-      zone: zone,
-      target: cdk.aws_route53.RecordTarget.fromAlias(new cdk.aws_route53_targets.CloudFrontTarget(cloudFrontDistribution)),
-    });
   }
 }
